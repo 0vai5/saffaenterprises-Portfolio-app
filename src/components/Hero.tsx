518210fe3935bbbacefb17d1933f22e38a060f9c
Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-04-02 15:52:28 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,14 +4,14 @@ import { Button } from "./ui/button";
 const Hero = () => {
   return (
     <section className="mb-10 h-[70vh] bg-center bg-no-repeat bg-[url(https://images.unsplash.com/photo-1655122878062-a9e885391a1b?q=80&w=1476&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)]">
-      <div className="bg-slate-600/50 pt-10 h-full flex flex-col md:gap-3 gap-2 justify-center items-center">
+      <div className="bg-slate-600/50 pt-7 h-full flex flex-col md:gap-3 gap-2 justify-center items-center">
         <p className="text-center font-semibold text-white">
           Dyeing and Bleaching Machinery Parts.
         </p>
         <h1 className="head-text text-center text-white">
           Quality Parts, Seamless Performance
         </h1>
-        <p className="text-center font-semibold md:text-white text-black">
+        <p className="text-center font-semibold md:py-0 py-5 md:text-white text-black">
           Discover top-quality dyeing and bleaching machinery parts,
           meticulously crafted to ensure peak performance
         </p>
