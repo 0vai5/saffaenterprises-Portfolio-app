6ecde881d7a2c1a82b4c96061274e20ccf538693
Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-04-02 15:45:51 +0500

    Update Merquee.tsx

--- src/components/Merquee.tsx
+++ src/components/Merquee.tsx
@@ -6,7 +6,7 @@ const Merquee = () => {
     // TODO: Making all the image Links in a constant file and importing and maping
     <section className="max-w-6xl mx-auto  px-8 min-h-[20vh] text-center flex items-center flex-col">
         <h1 className="subhead-text mb-10">Trusted <span className="text-[#1065DC]">Clients</span></h1>
-      <Marquee speed={150} gradient={true}>
+      <Marquee speed={60} gradient={true}>
         <div className="flex gap-[5rem] items-center justify-center">
         {Clients.map((client, index) => (
         <img key={index} src={client.logo} alt={client.name} className={client.classes} />
